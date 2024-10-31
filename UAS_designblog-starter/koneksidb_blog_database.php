180be76ec5f059bb75807d6523309d0d5ebb3847
atharaditya <athanala13@gmail.com>
2024-10-31 14:25:56 +0700

    buat database

---
+++ UAS_designblog-starter/koneksidb_blog_database.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost'; // Ganti dengan host database Anda
+$db = 'blog_database'; // Nama database yang telah dibuat
+$user = 'root'; // Ganti dengan username database Anda
+$pass = ''; // Ganti dengan password database Anda
+
+// Membuat koneksi
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Memeriksa koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
